4ec975f298e6a9062bfb4cebaa6b3d5b638b603d
dang-mang <collinmuurphy@gmail.com>
2021-02-17 22:10:07 -0700

    first commit with readme and client

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SERVER = '45.77.215.116'
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Connecting to server...")
+client.connect(ADDR)
+print("Connection successful.")
+
+def send(msg):
+	msg = "\n" + msg
+	message = msg.encode(FORMAT)
+	msg_length = len(message)
+	send_length = str(msg_length).encode(FORMAT)
+	send_length += b' ' * (HEADER - len(send_length))
+	client.send(send_length)
+	client.send(message)
+
+username = input("Input your username here: ")
+
+msg = None
+while msg != DISCONNECT_MESSAGE:
+	msg = input(">")
+	if msg != DISCONNECT_MESSAGE:
+		send(username+ ': ' + msg)
+		message = client.recv(2048).decode(FORMAT).replace("\n", "")
+		print(message)
