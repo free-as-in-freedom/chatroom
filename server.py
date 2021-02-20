de4da4f4bc6a8b4150453a3d0c67c3589a8ddec2
root <root@guest.guest>
2021-02-20 20:29:43 +0000

    improved server print output

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 from ip_info import SERVER
 
+#Global variables
 HEADER = 64
 PORT = 5050
 #get your local ipv4 address
@@ -9,12 +10,14 @@ ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
+#Set up server at IP address
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(ADDR)
 
+#What to do when a new client connects
 def handle_client(conn, addr, my_clients):
-    #print(f"[NEW CONNECTION] {addr} connected.")
+    #add their conn to a list of all connections
     my_clients.append(conn)
     connected = True
     while connected:
@@ -22,26 +25,30 @@ def handle_client(conn, addr, my_clients):
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
+            contents = msg.split(": ")[1]
+            #what to do on disconnect
+            if contents == DISCONNECT_MESSAGE:
                 connected = False
                 my_clients.remove(conn)
-                print(conn, addr, "\n")
             print(msg.replace("\n", ""))
             send_all(msg.encode(FORMAT), my_clients)
-            
+
+#send a message to all connected clients
 def send_all(message, my_clients):
     for client in my_clients:
         client.send(message)
+
+#start server and thread the different users
 def start():
     my_clients = []
     server.listen()
-    print(f"[LISTENING] Chatroom is online.")
+    print(f"[LISTENING] Chatroom is online at {SERVER}.")
     while True:
         conn, addr = server.accept()
         my_clients.append(conn)
+        #start a thread of handleclient() with each new user
         thread = threading.Thread(target=handle_client, args=(conn, addr, my_clients))
         thread.start()
-        #print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 print("[STARTING] Server is starting...")
 start()
