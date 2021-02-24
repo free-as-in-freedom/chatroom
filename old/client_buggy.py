0a1d88b01ba99ebfb0a4169d51081151da12bfe4
root <root@guest.guest>
2021-02-24 09:23:00 +0000

    added some buggy WIP code into the old directory

---
+++ old/client_buggy.py
@@ -0,0 +1,78 @@
+import socket
+import threading
+import os
+import time
+from ip_info import SERVER
+
+#global variables
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!D'
+#get user input for IP address or URL
+#SERVER = input("Enter server URL/IP ADDRESS: ")
+ADDR = (SERVER, PORT)
+
+def send(msg):
+	msg = "\n" + msg
+	message = msg.encode(FORMAT)
+	msg_length = len(message)
+	send_length = str(msg_length).encode(FORMAT)
+	send_length += b' ' * (HEADER - len(send_length))
+	client.send(send_length)
+	client.send(message)
+	if DISCONNECT_MESSAGE == msg.split(": ")[1]:
+		print("You have disconnected.")
+		os._exit(0)	
+
+def read_messages(username):
+	while True:
+		message = client.recv(2048).decode(FORMAT).replace("\n", "")
+		#message = client.recv(2048).decode(FORMAT)
+		user = message.split(":")[0]
+		current_time = getTime()
+		if DISCONNECT_MESSAGE == message.split(": ")[1]:
+			print(f"\n{current_time} {user} has disconnected.", end = "\n>")
+		else:
+			print(f"\n{current_time} {message}", end = "\n>")
+
+def getTime():
+	#Time variable	
+	hours = str(time.localtime().tm_hour)
+	mins = str(time.localtime().tm_min)
+	if int(mins) < 10:
+		mins = '0' + mins
+	secs = str(time.localtime().tm_sec)
+	if int(secs) < 10:
+		secs = '0' + secs
+	return '[' + hours + ':' + mins + ':' + secs + ']'
+
+def main():
+	username = input("Input your username here: ")
+	print(f"Welcome to the chatroom, {username}!")	
+	msg = None	
+	#create thread for reading messages so everything can be done in real time
+	thread = threading.Thread(target = read_messages, args = (username, ))
+	thread.start()
+	
+	first = True
+	
+	#user input and sending messages
+	while True:
+		#if else is for display purposes
+		if first == True:
+			msg = input(">")
+			first = False
+		else:
+			msg = input("")
+		send(username+ ': ' + msg)
+
+
+#join server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(f"Connecting to {SERVER}...")
+client.connect(ADDR)
+print(f"Connection to {SERVER} successful.")
+
+main()
+
---
+++ old/server_buggy.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+from ip_info import SERVER
+
+#Global variables
+HEADER = 64
+PORT = 5050
+#get your local ipv4 address
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!D"
+
+#Set up server at IP address
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(ADDR)
+
+#What to do when a new client connects
+def handle_client(conn, addr, my_clients):
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            contents = msg.split(": ")[1]
+            #what to do on disconnect
+            if contents == DISCONNECT_MESSAGE:
+                connected = False
+                my_clients.remove(conn)
+            print(msg.replace("\n", ""))
+            send_all(msg.encode(FORMAT), my_clients, conn)
+
+#send a message to all connected clients
+def send_all(message, my_clients, conn):
+    print(len(my_clients))
+    for client in my_clients:
+        if conn == client:            
+            message = "!user" + message.decode(FORMAT)
+            client.send(message.encode(FORMAT))
+        else:
+            if isinstance(message, str):
+                client.send(message.encode(FORMAT))
+
+#start server and thread the different users
+def start():
+    my_clients = []
+    server.listen()
+    print(f"[LISTENING] Chatroom is online at {SERVER}.")
+    while True:
+        #add their conn to a list of all connections
+        conn, addr = server.accept()
+        my_clients.append(conn)
+        #start a thread of handleclient() with each new user
+        thread = threading.Thread(target=handle_client, args=(conn, addr, my_clients))
+        thread.start()
+
+
+print("[STARTING] Server is starting...")
+start()
+
