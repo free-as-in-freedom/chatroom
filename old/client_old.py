ec2e29c5ed8a80f0b19c08e4a8327e9fff267096
dang-mang <collinmuurphy@gmail.com>
2021-02-24 01:18:24 -0700

    clientside new stuff

--- old/client_old.py
+++ old/client_old.py
@@ -1,16 +1,13 @@
 import socket
 import threading
-import os
-import time
-from ip_info import SERVER
 
 #global variables
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = '!D'
+DISCONNECT_MESSAGE = '!DISCONNECT'
 #get user input for IP address or URL
-#SERVER = input("Enter server URL/IP ADDRESS: ")
+SERVER = input("Enter server URL/IP ADDRESS: ")
 ADDR = (SERVER, PORT)
 
 def send(msg):
@@ -21,31 +18,24 @@ def send(msg):
 	send_length += b' ' * (HEADER - len(send_length))
 	client.send(send_length)
 	client.send(message)
-	if DISCONNECT_MESSAGE == msg.split(": ")[1]:
-		print("You have disconnected.")
-		os._exit(0)	
 
 def read_messages(username):
+	prev_message = None
 	while True:
 		message = client.recv(2048).decode(FORMAT).replace("\n", "")
-		#message = client.recv(2048).decode(FORMAT)
-		user = message.split(":")[0]
-		current_time = getTime()
-		if DISCONNECT_MESSAGE == message.split(": ")[1]:
-			print(f"\n{current_time} {user} has disconnected.", end = "\n>")
-		else:
-			print(f"\n{current_time} {message}", end = "\n>")
-
-def getTime():
-	#Time variable	
-	hours = str(time.localtime().tm_hour)
-	mins = str(time.localtime().tm_min)
-	if int(mins) < 10:
-		mins = '0' + mins
-	secs = str(time.localtime().tm_sec)
-	if int(secs) < 10:
-		secs = '0' + secs
-	return '[' + hours + ':' + mins + ':' + secs + ']'
+		other_user = message.split(":")[0]
+		if message != prev_message:
+			if ": !DISCONNECT" in message:
+				if other_user == username:
+					print(f"You has disconnected.")
+					return	
+				else:
+					print(f"\n{other_user} has disconnected.", end = "\n>")
+			elif message.split(":")[0] == username:
+				print(message, end = "\n>")
+			else:
+				print(f"\n{message}", end = "\n>")
+		prev_message = message		
 
 def main():
 	username = input("Input your username here: ")
