0b2d502318fbfb8f9bceac37a72f044090e0f132
root <root@guest.guest>
2021-02-24 08:15:00 +0000

    better server

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ PORT = 5050
 #get your local ipv4 address
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = "!D"
 
 #Set up server at IP address
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,8 +17,6 @@ server.bind(ADDR)
 
 #What to do when a new client connects
 def handle_client(conn, addr, my_clients):
-    #add their conn to a list of all connections
-    my_clients.append(conn)
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
@@ -31,12 +29,19 @@ def handle_client(conn, addr, my_clients):
                 connected = False
                 my_clients.remove(conn)
             print(msg.replace("\n", ""))
-            send_all(msg.encode(FORMAT), my_clients)
+            send_all(msg.encode(FORMAT), my_clients, conn)
 
 #send a message to all connected clients
-def send_all(message, my_clients):
+def send_all(message, my_clients, conn):
+    print(len(my_clients))
     for client in my_clients:
-        client.send(message)
+        if conn != client:
+            #message = '\n' + message.decode(FORMAT)
+            #message = message.encode(FORMAT)
+            client.send(message)
+        else:
+            #message = (message.decode(FORMAT).replace("\n","").encode(FORMAT))
+            client.send(message)
 
 #start server and thread the different users
 def start():
@@ -44,11 +49,13 @@ def start():
     server.listen()
     print(f"[LISTENING] Chatroom is online at {SERVER}.")
     while True:
+        #add their conn to a list of all connections
         conn, addr = server.accept()
         my_clients.append(conn)
         #start a thread of handleclient() with each new user
         thread = threading.Thread(target=handle_client, args=(conn, addr, my_clients))
         thread.start()
+
 
 print("[STARTING] Server is starting...")
 start()
